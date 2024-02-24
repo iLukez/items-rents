8c9945e66bc388ad924b721ab369f4142ff04dc5
iLukez <luca.donadel05@gmail.com>
2024-02-24 13:01:10 +0100

    updated database

--- rent.sql
+++ rent.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Feb 24, 2024 alle 11:57
+-- Creato il: Feb 24, 2024 alle 13:00
 -- Versione del server: 10.4.8-MariaDB
 -- Versione PHP: 7.3.10
 
@@ -25,14 +25,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struttura della tabella `centers`
+--
+
+CREATE TABLE `centers` (
+  `center_id` int(16) NOT NULL,
+  `nome` varchar(32) DEFAULT NULL,
+  `indirizzo` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Struttura della tabella `items`
 --
 
 CREATE TABLE `items` (
-  `id` bigint(16) NOT NULL,
+  `item_id` int(16) NOT NULL,
+  `type` varchar(16) DEFAULT NULL,
   `name` varchar(32) DEFAULT NULL,
   `maker` varchar(32) DEFAULT NULL,
-  `ci` int(8) UNSIGNED ZEROFILL DEFAULT NULL
+  `center_id` int(16) NOT NULL,
+  `ci` int(8) UNSIGNED ZEROFILL DEFAULT NULL,
+  `state` varchar(16) NOT NULL DEFAULT 'disponibile'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -42,9 +57,9 @@ CREATE TABLE `items` (
 --
 
 CREATE TABLE `rents` (
-  `id` bigint(16) NOT NULL,
-  `fk_user` int(16) NOT NULL,
-  `fk_item` int(16) NOT NULL,
+  `rent_id` int(16) NOT NULL,
+  `user_id` int(16) NOT NULL,
+  `item_id` int(16) NOT NULL,
   `date` datetime(6) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -55,7 +70,7 @@ CREATE TABLE `rents` (
 --
 
 CREATE TABLE `users` (
-  `id` bigint(16) UNSIGNED NOT NULL,
+  `user_id` int(16) NOT NULL,
   `username` varchar(32) NOT NULL,
   `email` varchar(32) NOT NULL,
   `password` varchar(32) NOT NULL,
@@ -67,44 +82,47 @@ CREATE TABLE `users` (
 --
 
 --
+-- Indici per le tabelle `centers`
+--
+ALTER TABLE `centers`
+  ADD PRIMARY KEY (`center_id`);
+
+--
 -- Indici per le tabelle `items`
 --
 ALTER TABLE `items`
-  ADD PRIMARY KEY (`id`);
+  ADD UNIQUE KEY `item_id` (`item_id`),
+  ADD KEY `center_id` (`center_id`);
 
 --
 -- Indici per le tabelle `rents`
 --
 ALTER TABLE `rents`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`rent_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `item_id` (`item_id`) USING BTREE;
 
 --
 -- Indici per le tabelle `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`user_id`);
 
 --
 -- AUTO_INCREMENT per le tabelle scaricate
 --
 
 --
--- AUTO_INCREMENT per la tabella `items`
---
-ALTER TABLE `items`
-  MODIFY `id` bigint(16) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT per la tabella `rents`
 --
 ALTER TABLE `rents`
-  MODIFY `id` bigint(16) NOT NULL AUTO_INCREMENT;
+  MODIFY `rent_id` int(16) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT per la tabella `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(16) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(16) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
